276044d8b8efe8e0fe80bf9bb980af3f938a1f24
Juan Rezende Lobo <deviant.lobo@gmail.com>
2026-01-05 20:27:24 -0300

    fix(flow): remove redundant fields from report findings

--- frontend/app/report-findings/page.tsx
+++ frontend/app/report-findings/page.tsx
@@ -7,13 +7,8 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
 import Container from "@mui/material/Container";
-import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormLabel from "@mui/material/FormLabel";
 import IconButton from "@mui/material/IconButton";
 import Paper from "@mui/material/Paper";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
@@ -33,9 +28,7 @@ export default function ReportFindingsPage() {
     examType,
     templateId,
     indication,
-    setIndication,
     contrast,
-    setContrast,
     findings,
     setFindings,
     setReportText,
@@ -134,27 +127,6 @@ export default function ReportFindingsPage() {
           <Typography variant="body2" color="text.secondary">
             Template: {templateId ?? "-"}
           </Typography>
-
-          <TextField
-            label="Indicação"
-            fullWidth
-            multiline
-            minRows={2}
-            value={indication}
-            onChange={(e) => setIndication(e.target.value)}
-          />
-
-          <FormControl>
-            <FormLabel>Contraste</FormLabel>
-            <RadioGroup
-              row
-              value={contrast}
-              onChange={(e) => setContrast(e.target.value as "with" | "without")}
-            >
-              <FormControlLabel value="with" control={<Radio />} label="COM" />
-              <FormControlLabel value="without" control={<Radio />} label="SEM" />
-            </RadioGroup>
-          </FormControl>
 
           <TextField
             label="Achados"
